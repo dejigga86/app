c0eaadd418095f10329e1d5cceaee2804fddead2
dejigga86 <tobiasw13@gmx.net>
2016-06-01 13:16:18 +0200

    Add files via upload

---
+++ main/java/com/example/tobias/myapplication/navi.java
@@ -0,0 +1,21 @@
+package com.example.tobias.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Tobias on 29.05.2016.
+ */
+public class navi extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.navigation);
+    }
+
+}
+
+
